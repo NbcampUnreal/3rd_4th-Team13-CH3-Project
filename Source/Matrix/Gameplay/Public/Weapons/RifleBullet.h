209ad25574469c606b6f8e91fde2b6f34d563803
AmericanoTwoshot <uji06134@naver.com>
2025-07-30 21:39:03 +0900

    Feat: 기본 무기, 기본 총알 생성
    WeaponBase : 기본 무기 조상 클래스
    BulletBase : 기본 총알 조상 클래스
    Rifle : 소총 무기 예시
    RifleBullet : 소총 총알 예시

---
+++ Source/Matrix/Gameplay/Public/Weapons/RifleBullet.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WeaponSystem/BulletBase.h"
+#include "RifleBullet.generated.h"
+
+UCLASS()
+class MATRIX_API ARifleBullet : public ABulletBase
+{
+	GENERATED_BODY()
+	
+public:	
+	ARifleBullet();
+	
+};
