d8c4c2bf699ab2c97143f813db5955ba543dca78
YouKnowGit <choi15ho@gmail.com>
2025-07-31 14:12:37 +0900

    Feat: 모듈 의존성 업데이트 및 불필요한 코드 정리
    엔진 모듈 의존성을 명시적으로 추가하고 코드 정리를 통해 불필요한 주석 및 설정 제거.
    
    - Matrix.uproject에 엔진 모듈(Engine) 종속성 추가.
    - `Matrix.Build.cs` 파일에서 사용되지 않는 모듈 의존성 관련 주석 제거.
    - 헤더 검색 경로 설정과 관련된 코드 정리로 가독성 개선.

--- Source/Matrix/Matrix.Build.cs
+++ Source/Matrix/Matrix.Build.cs
@@ -10,22 +10,7 @@ public class Matrix : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NavigationSystem", "AIModule", "GameplayTasks", "UMG", "Slate", "SlateCore" });
-
-		// 게임플레이 어빌리티 시스템(GAS) 사용 시 활성화
-		// PublicDependencyModuleNames.Add("GameplayAbilities");
-		
-		PrivateDependencyModuleNames.AddRange(new string[] { "Gameplay", "UI", "GameMode", "AI" });
-		
-		// 헤더 검색 경로 설정
-		PublicIncludePaths.AddRange(new string[] {
-			Path.Combine(ModuleDirectory, "Core/Public"),
-			Path.Combine(ModuleDirectory, "Gameplay/Public"),
-			Path.Combine(ModuleDirectory, "UI/Public"),
-			Path.Combine(ModuleDirectory, "GameMode/Public"),
-			Path.Combine(ModuleDirectory, "InventorySystem/Public"),
-			Path.Combine(ModuleDirectory, "AI/Public")
-		});
-		
+				
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
